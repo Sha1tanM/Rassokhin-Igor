ff950c5780a716acc568ebdcee41fa2dba065b4b
Sha1tanM <97942261+Sha1tanM@users.noreply.github.com>
2022-03-22 15:15:31 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32 (textBox1.Text);
+            int b= Convert.ToInt32 (textBox2.Text);
+            int c = Convert.ToInt32 (textBox3.Text);
+            int x = a + b;
+            int y = a + c;
+            int z = b + c;
+            if (x > c && y > b && z > a)
+             MessageBox.Show("Треугольник реализуется");
+            else
+                MessageBox.Show("Треугольник не реализуется");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(textBox3.Text);
+            int x = a + b;
+            int y = a + c;
+            int z = b + c;
+            if (x > c && y > b && z > a)
+            {
+                if (a == b && a == c && b == c)
+                {
+                    MessageBox.Show("Треугольник равносторонний");
+                }
+                if (a == b || a == c || b == c)
+                {
+                    MessageBox.Show("Треугольник равнобедренных");
+                }
+                else { MessageBox.Show("Треугольник равносторонний"); }
+
+            }
+            else { MessageBox.Show("Треугольник не реализуется"); }
+  }
